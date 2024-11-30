9077e95372b44d587a4443f41fa91af21f74653b
livi5599 <obrown1@uwyo.edu>
2024-11-30 16:17:44 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,28 @@
 function mergesort(array) {
     if (array.length == 0)
-        return 0;
+        return array;
     if (array.length == 1)
         return array[0];
 
     for (width = 1; width < array.length; width *= 2) {
         for (i = 0; i < array.length; i += 2 * width) {
+            console.log("i = ", i);
             var low = i;
-            var high = 2 * width;
-            var mid = Math.floor((high - low) / 2);
-            for (j = low; j <= mid; j++) {
-                for (k = high; k >= mid; k--) {
-                    if (array[k] < array[j]) {
-                        [array[j], array[k]] = [array[k], array[j]];
-                    }
-                }
+            var mid = i + width;
+            var high = i + 2 * width;
+
+            if (mid > array.length) {
+                console.log("Breaking out of for loop");
+                break;
             }
+
+            if (high > array.length) {
+                high = array.length;
+            }
+
+            merge(array, low, mid, high);
         }
     }
+
     return array;
 }
