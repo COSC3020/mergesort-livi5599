f38ba1b6e874759f3951d21112efc7761803475b
livi5599 <obrown1@uwyo.edu>
2024-12-02 11:53:47 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ function mergesort(array) {
     if (array.length == 0)
         return array;
     if (array.length == 1)
-        return array[0];
+        return array;
 
     for (width = 1; width < array.length; width *= 2) {
         for (i = 0; i < array.length; i += 2 * width) {
